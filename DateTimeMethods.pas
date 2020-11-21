d319cd3dc80ac546ba70236718003739fb29df2e
vadim <vadim-ivanov-20010510@mail.ru>
2020-11-21 21:42:13 +0300

    Revert "add DaysInMonth"
    This reverts commit ff70709a01628b6cfd56e7d051ad2e661482fb80.

--- DateTimeMethods.pas
+++ DateTimeMethods.pas
@@ -6,15 +6,7 @@ begin
   If year mod 4 = 0 then
     Result := True;
 end;
-function DaysInMonth(month, year: integer): integer;
-begin
-  case month of
-    1, 3, 5, 7, 8, 10, 12 : Result := 31;
-    2 : Result := 28;
-    4, 6, 9, 11 : Result := 30;
-    else Result := 0;
-  end;
-end;
+function DaysInMonth(month, year: integer): integer := 0;
 function LaterInDay(p1, p2: DateTime): DateTime := p1;
 function LaterInYear(p1, p2: DateTime): DateTime := p1;
 function DaysInYear(year: integer): integer := 0;
